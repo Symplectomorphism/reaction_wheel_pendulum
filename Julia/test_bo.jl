de7eb149c8f02dee3fefd77d77c4e7959086dc8f
Aykut C. Satici <acsatici@gmail.com>
2021-06-13 20:17:18 -0600

    little test script provided.

---
+++ Julia/test_bo.jl
@@ -0,0 +1,24 @@
+include("iwp.jl")
+
+env = IWPEnv()
+x0 = [1*π/180, 0.5, 0, 0]
+simulate!(env, x0)
+
+A, B, K, S = lqr_gains(env; Q=env.policy.Q, R=env.policy.R);
+
+
+function test_bo_vs_lqr()
+    res, opt = find_gains_BO(env)
+
+    unc_lqr_loss = simulate!(env, x0, K)
+    bo_loss = simulate!(env, x0, res.observed_optimizer)
+
+    while bo_loss > unc_lqr_loss && opt.iterations.i < 600
+        res = boptimize!(opt)
+        bo_loss = simulate!(env, x0, res.observed_optimizer)
+    end
+    return bo_loss, unc_lqr_loss
+end
+
+bo_loss, unc_lqr_loss = test_bo_vs_lqr()
+@assert bo_loss <= unc_lqr_loss
