58a2dc77c93d5f7b4605274c578107201ef513da
Aykut Satici <acsatici@gmail.com>
2021-06-14 11:46:52 -0600

    some more improvements

--- Julia/test_bo.jl
+++ Julia/test_bo.jl
@@ -7,18 +7,22 @@ simulate!(env, x0)
 A, B, K, S = lqr_gains(env; Q=env.policy.Q, R=env.policy.R);
 
 
-function test_bo_vs_lqr()
+function test_bo_vs_lqr(;iters::Int=1000, reset::Bool=false)
     res, opt = find_gains_BO(env; maxiters=100)
 
     unc_lqr_loss = simulate!(env, x0, K)
     bo_loss = simulate!(env, x0, res.observed_optimizer)
 
-    while bo_loss > unc_lqr_loss && opt.iterations.i < 2000
-        res = boptimize!(opt)
+    while bo_loss > unc_lqr_loss && opt.iterations.i < iters
+        if reset
+            res, opt = find_gains_BO(env; maxiters=100) 
+        else 
+            res = boptimize!(opt)
+        end
         bo_loss = simulate!(env, x0, res.observed_optimizer)
     end
-    return bo_loss, unc_lqr_loss
+    return bo_loss, unc_lqr_loss, res
 end
 
-bo_loss, unc_lqr_loss = test_bo_vs_lqr()
+bo_loss, unc_lqr_loss, res = test_bo_vs_lqr(iters=1000, reset=true)
 @assert bo_loss <= unc_lqr_loss
